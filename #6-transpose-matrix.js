a253ca2e94b8d462f92e95da9238b244ff8d7144
francky <francky@tuta.io>
2023-01-17 00:29:32 +0100

    not working in codewars test

--- #6-transpose-matrix.js
+++ #6-transpose-matrix.js
@@ -5,24 +5,38 @@ function transpose(...matrix) {
   let result = [];
 
   matrix
+  items
+  nRows
+  nCols
 
   matrix.forEach(row => {
     row.forEach( r => {
       items.push(r);
     })  
   })
-  
-  nRows
-  nCols
 
-  for (let k = 0; k < nRows; k++) {
+  if (matrix.length < 2) {
+    matrix[0].forEach((item, index) => {
+      if (item === undefined) { matrix[0].pop(matrix[0][index]) }
+    })
+
     let row = [];
     for (let i = 0; i <= nCols; i += nRows) {
-      row.push(items[k]);
-      row.push(items[k+nRows]);
+      row.push(items[i]);
     }
-    row
     result.push(row);
+  }
+  else {
+    for (let k = 0; k < nRows; k++) {
+      let row = [];
+      for (let i = 0; i <= nCols; i += nRows) {
+        row.push(items[k]);
+        row.push(items[k+nRows]);
+      }
+      row.forEach( r => { if (r == undefined) row.pop(r) } );
+      row
+      result.push(row);
+    }
   }
 
   result
@@ -30,7 +44,10 @@ function transpose(...matrix) {
 
 }
 
-transpose( [ 1, 2, 1, 2 ] );
+transpose( ['a','b','c'],['d','e','f'] );
 
-// [ ['a','b','c'],['d','e','f'] ]  ==>  [ [a,d],[b,e],[c,f] ]
+// [ 1, undefined ]
+// [ [ 1, undefined, 1, undefined ] ]
+// [ 1, 2, 1, 2 ]
+// [ ['a','b','c'],['d','e','f'] ]  ==>  [ ['a','d'],['b','e'],['c','f'] ]
 // [ [1,2,3],[4,5,6] ]  ==>  [ [1,4],[2,5],[3,6] ]
