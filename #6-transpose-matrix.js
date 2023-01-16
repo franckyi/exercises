0a1b69ee47518d7c441cae9c53f9aad8d0e253a3
francky <francky@tuta.io>
2023-01-16 23:54:26 +0100

    update

--- #6-transpose-matrix.js
+++ #6-transpose-matrix.js
@@ -1,41 +1,36 @@
 function transpose(...matrix) {
-  matrix
-
   let nRows = matrix[0].length;
   let nCols = matrix.length;
   let items = [];
   let result = [];
 
-  nRows
-  nCols
+  matrix
 
   matrix.forEach(row => {
     row.forEach( r => {
       items.push(r);
     })  
   })
-  items
+  
+  nRows
+  nCols
 
-  items.forEach( (item, index) => {
+  for (let k = 0; k < nRows; k++) {
     let row = [];
-    index
-    item
-    // for (let i = index; i < nCols;) {
-    //   // if (item[i] !== undefined) {
-    //     row.push(item);
-    //   // }
-    //   i = i + nRows;
-    // }
+    for (let i = 0; i <= nCols; i += nRows) {
+      row.push(items[k]);
+      row.push(items[k+nRows]);
+    }
     row
     result.push(row);
-  })
+  }
 
   result
   return result;
 
 }
 
-transpose( [1,2,3], [4,5,6] );
+transpose( [ 1, 2, 1, 2 ] );
 
-// [ [a,b,c],[d,e,f] ]  ==>  [ [a,d],[b,e],[c,f] ]
+// [ ['a','b','c'],['d','e','f'] ]  ==>  [ [a,d],[b,e],[c,f] ]
 // [ [1,2,3],[4,5,6] ]  ==>  [ [1,4],[2,5],[3,6] ]
