65097a9a339c886e83bcf83f0e1a25f6ce82b2f4
francky <francky@igamingnuts.com>
2023-01-18 23:14:36 +0100

    in progress

--- #6-transpose-matrix.js
+++ #6-transpose-matrix.js
@@ -1,37 +1,46 @@
 function transpose(...matrix) {
-
+  
   let items = [];
   let temp = [];
   let result = [];
+
+  console.log(matrix);
   
+  // PUSH MATRIX TO TEMP
   matrix.forEach(row => {
-    row.forEach( r => {
-      if (typeof r !== 'undefined') { temp.push(r) };
-    })  
-  })
-  
-  temp.forEach( value => {
-    for (let i = 0; i < value.length; i++) {
-      console.log(value[i]);
-      if (typeof value[i] !== 'undefined') {
-        items.push(value[i])
-      };
-    }
+    row.forEach( r => { console.log(row); if (r !== undefined) temp.push(r) });  
   })
 
+  // TEMP CONTAINS INITIAL ROWS
+  console.log(temp);
+  console.log(temp.length);
+  
+  
+  // ACCESS TEMP VALUES
+    temp.forEach( t => {
+    for (let i = 0; i < t.length; i++) {
+      console.log(t[i]);
+        items.push(t[i])
+      }
+    });
+
+  // CHECK IF nRows EQUALS temp[0].length or matrix.length ?
   let nRows = temp[0].length;
   let nCols = temp.length;
   
-  matrix
-  temp
-  nRows
-  nCols
-  items
+  temp[0] = temp[0].filter( t => { return t!== undefined } );
+  items = items.filter( i => { return i!== undefined } );
+
+  console.log(matrix);
+  console.log(temp);
+  console.log(nRows);
+  console.log(nCols);
+  console.log(items);
 
   if (temp.length > 0 && temp.length < 2) {
-    temp[0].forEach((m, index) => {
-      if (typeof m === 'undefined') { temp[0].pop(temp[0][index]) }
-    })
+    // temp[0].forEach((m, index) => {
+      // if (typeof m === 'undefined') { temp[0].pop(temp[0][index]) }
+    // })
 
     let row = [];
     for (let i = 0; i <= nCols; i += nRows) {
@@ -39,6 +48,7 @@ function transpose(...matrix) {
     }
     result.push(row);
   }
+  
   else if (temp.length >= 2) {
     for (let k = 0; k < nRows; k++) {
       let row = [];
@@ -46,12 +56,13 @@ function transpose(...matrix) {
         row.push(items[k]);
         row.push(items[k+nRows]);
       }
-      row.forEach( r => { if (r == undefined) row.pop(r) } );
+      // row.forEach( r => { if (r === undefined) row.pop(r) } );
+      row = row.filter( r => {return r!== undefined} );
       result.push(row);
     }
   }
 
-  result
+  console.log(result);
   return result;
 
 }
