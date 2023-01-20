6a63ef4285f944f0fd35a87a3614a444a908738f
francky <francky@tuta.io>
2023-01-20 22:14:24 +0100

    add new

---
+++ #3-sudoku-solver.js
@@ -0,0 +1,27 @@
+function sudoku(puzzle) {
+  //return the solved puzzle as a 2d array of 9 x 9 
+
+  /*
+  
+  first create cols (we already have rows)
+  second find rows and cols with more filled numbers
+  
+  */
+
+
+
+}
+
+var puzzle = [
+  /* 0 */ [5,3,0,0,7,0,0,0,0],
+  /* 1 */ [6,0,0,1,9,5,0,0,0],
+  /* 2 */ [0,9,8,0,0,0,0,6,0],
+  /* 3 */ [8,0,0,0,6,0,0,0,3],
+  /* 4 */ [4,0,0,8,0,3,0,0,1],
+  /* 5 */ [7,0,0,0,2,0,0,0,6],
+  /* 6 */ [0,6,0,0,0,0,2,8,0],
+  /* 7 */ [0,0,0,4,1,9,0,0,5],
+  /* 8 */ [0,0,0,0,8,0,0,7,9]
+  /*       0 1 2 3 4 5 6 7 8
+  */
+];
