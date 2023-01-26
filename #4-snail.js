27342f52d25658814cfc76e69e7833c942462cbf
francky <francky@tuta.io>
2023-01-27 00:16:05 +0100

    in progress

--- #4-snail.js
+++ #4-snail.js
@@ -7,39 +7,39 @@ snail = function(arr) {
 
   while (arr.length > 1) {
     let a = arr[0];
-    console.log(a);
-
     let b = [];
     arr.forEach( e => b.push(e[e.length-1]) );
-    console.log(b);
-
     let c = arr[arr.length-1];
-    console.log(c);
-
     let d = [];
     arr.forEach( e => d.push(e[0]) );
+    
+    console.log(a);
+    console.log(b);
+    console.log(c);
     console.log(d);
 
     for(let i = 0; i < a.length-1; i++) {
       temp.push(a[i]);
     }
-    // console.log(temp);
+    console.log(temp);
   
     for(let i = 0; i < b.length-1; i++) {
       temp.push(b[i]);
     }
-    // console.log(temp);
+    console.log(temp);
 
     for(let i = c.length-1; i > 0; i--) {
       temp.push(c[i]);
     }
-    // console.log(temp);
+    console.log(temp);
   
     for(let i = d.length-1; i > 0; i--) {
       temp.push(d[i]);
     }    
+    
     console.log(temp);
-
+    console.log(arr);
+    
     arr.shift();
     arr.pop();
     arr.forEach( e => arr.unshift(e[0]) );
@@ -49,9 +49,9 @@ snail = function(arr) {
 
   }
 
+  if (arr.length == 1) temp.push(arr[0]);
+  
   console.log(temp);
-
-  // if (arr.length == 1) temp.push(arr[0]);
 
   // let ti = 0;
   // result.forEach( r => {
