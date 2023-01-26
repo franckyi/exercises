1513d080d8ce95c7e42dd33518354c79dc4cb936
francky <francky@tuta.io>
2023-01-27 00:04:50 +0100

    in progress

--- #4-snail.js
+++ #4-snail.js
@@ -1,50 +1,68 @@
 snail = function(arr) {
 
+  console.log(arr.length);
+
   let temp = [];
   let result = Array(arr.length);
 
-  while (arr.length > 1 && arr[0].length > 1) {
+  while (arr.length > 1) {
     let a = arr[0];
+    console.log(a);
+
     let b = [];
     arr.forEach( e => b.push(e[e.length-1]) );
+    console.log(b);
+
     let c = arr[arr.length-1];
+    console.log(c);
+
     let d = [];
     arr.forEach( e => d.push(e[0]) );
+    console.log(d);
 
-    for(let i = 0; i <= a.length-2; i++) {
+    for(let i = 0; i < a.length-1; i++) {
       temp.push(a[i]);
     }
-
-    for(let i = 0; i <= b.length-2; i++) {
+    // console.log(temp);
+  
+    for(let i = 0; i < b.length-1; i++) {
       temp.push(b[i]);
     }
+    // console.log(temp);
 
-    for(let i = c.length-1; i = 1; i--) {
+    for(let i = c.length-1; i > 0; i--) {
       temp.push(c[i]);
     }
-
-    for(let i = d.length-1; i = 1; i--) {
+    // console.log(temp);
+  
+    for(let i = d.length-1; i > 0; i--) {
       temp.push(d[i]);
     }    
+    console.log(temp);
 
     arr.shift();
     arr.pop();
-    arr.map( e, i => e[i].unshift() );
+    arr.forEach( e => arr.unshift(e[0]) );
+    arr.forEach( e => arr.pop(e[e.length-1]) );
+
+    console.log(arr);
 
   }
 
-  if (arr.length == 1) temp.push(arr[0]);
+  console.log(temp);
 
-  let ti = 0;
-  result.forEach( r => {
-    for(let i = 0; i < r.length; i++) {
-      r[i] = temp[ti];
-      ti++
-    }
-  });
+  // if (arr.length == 1) temp.push(arr[0]);
 
-  console.log(result);
-  return result;
+  // let ti = 0;
+  // result.forEach( r => {
+  //   for(let i = 0; i < r.length; i++) {
+  //     r[i] = temp[ti];
+  //     ti++
+  //   }
+  // });
+
+  // console.log(result);
+  // return result;
 
 }
 
