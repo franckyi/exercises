f50287f50634668f0a3a5b688d115a4eb501dfa2
francky <francky@igamingnuts.com>
2023-01-27 22:18:53 +0100

    solved

--- #4-snail.js
+++ #4-snail.js
@@ -1,18 +1,6 @@
 snail = function(arr) {
 
-  console.log(arr.length);
-  let length = arr.length;
-  console.log(length);
-
-  let temp = [];
-  let result = new Array(length);
-
-  for (let i = 0; i < length; i++) {
-    result[i] = new Array(arr[0].length);
-  }
-
-  console.log( Boolean(arr[0][0]) );
-  console.log(arr[0]);
+  let result = [];
 
   if (arr.length == 1) {
       console.log(arr[0]);
@@ -27,64 +15,34 @@ snail = function(arr) {
       let c = arr[arr.length-1];
       let d = [];
       arr.forEach( e => d.push(e[0]) );
-      
-      console.log(a);
-      console.log(b);
-      console.log(c);
-      console.log(d);
 
-      for(let i = 0; i < a.length-1; i++) {
-        temp.push(a[i]);
+      for (let i = 0; i < a.length-1; i++) {
+        result.push(a[i]);
       }
-      console.log(temp);
     
-      for(let i = 0; i < b.length-1; i++) {
-        temp.push(b[i]);
+      for (let i = 0; i < b.length-1; i++) {
+        result.push(b[i]);
       }
-      console.log(temp);
 
-      for(let i = c.length-1; i > 0; i--) {
-        temp.push(c[i]);
+      for (let i = c.length-1; i > 0; i--) {
+        result.push(c[i]);
       }
-      console.log(temp);
     
-      for(let i = d.length-1; i > 0; i--) {
-        temp.push(d[i]);
+      for (let i = d.length-1; i > 0; i--) {
+        result.push(d[i]);
       }    
       
-      console.log(temp);
-      console.log(arr);
-      
+    
       arr.shift();
-      console.log(arr);
       arr.pop();
-      console.log(arr);
       arr.forEach( a => a.shift() );
-      console.log(arr);
       arr.forEach( a => a.pop() );
-      console.log(arr);
       
-      console.log(arr);
-      console.log(arr[0]);
+      if (arr.length == 1) result.push(arr[0][0]);
 
-      if (arr.length == 1) temp.push(arr[0][0]);
-      console.log(temp);
     }
 
-    console.log(temp);
-    console.log(result);
-
-    let k = 0; 
-    result.forEach( r => {
-      
-      for (let i = 0; i < r.length; i++, k++) {
-        console.log(temp[k]);
-        r[i] = temp[k];
-      }
-
-    });
-
-    console.log(result);
+    // console.log(result);
     return result;
   }
 
