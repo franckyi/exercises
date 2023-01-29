c3fb7fffbedcb46034b74ab6cbbc6bedb4c8ff0e
francky <francky@igamingnuts.com>
2023-01-29 10:33:20 +0100

    test

---
+++ test.js
@@ -0,0 +1,19 @@
+const val = new class
+
+
+// // arr1.sameStructureAs(arr2);
+
+// const arr1 = {
+//   arr1: function() {
+//     console.log('test');
+//     // return 15
+//   },
+  
+//   sameValueAs: function(arr1, arr2) {
+//     console.log(`YES, is the same as ${arr2}`);
+//     console.log(`NOPE, is not the same as ${arr2}`);
+//   },
+// }
+
+// console.log(arr1.arr1);
+// // arr1.sameValueAs(15);
