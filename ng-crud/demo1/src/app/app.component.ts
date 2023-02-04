a787425f575ef899f7dfde9d109407a749ea3fd3
francky <francky@igamingnuts.com>
2023-02-05 00:37:03 +0100

    in progress

--- ng-crud/demo1/src/app/app.component.ts
+++ ng-crud/demo1/src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
   template: `
-
+  <div class="container">
+    <h1>Users</h1>
+    <ul class="list-group">
+    <li *ngFor="let u of users" class="list-group-item"
+      [ngClass]="{
+        'male': u.gender === 'M',
+        'female': u.gender === 'F',
+      }"
+    >
+      <i class="fa fa-3x"
+        [ngClass]="{
+          'fa-mars': u.gender === 'M',
+          'fa-venus': u.gender === 'F'
+        }"
+        ></i>
+      {{u.label}}
+    </li>
+    </ul>
+  </div>
   `,
-  styles: []
+  styles: [`
+  .male { background-color: #36caff; }
+  .female { background-color: pink; }
+`]
 })
 export class AppComponent {
-  title = 'demo1';
+  users: User[] = [                                 // list of users
+  { id: 1, label: 'Fabio', gender: 'M', age: 20 },
+  { id: 2, label: 'Lorenzo', gender: 'M', age: 37 },
+  { id: 3, label: 'Silvia', gender: 'F', age: 70 },
+];
 }
---
+++ ng-crud/demo1/src/app/model/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+  id: number;
+  label: string;
+  gender: string;
+  age: number;
+}
