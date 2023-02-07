7b04dc23a9e19bcf361f4a9020f2184c3bcd6313
francky <francky@igamingnuts.com>
2023-02-07 12:27:01 +0100

    in progress

--- #3-sudoku-solver.js
+++ #3-sudoku-solver.js
@@ -1,36 +1,19 @@
 function sudoku(puzzle) {
-  //return the solved puzzle as a 2d array of 9 x 9 
-  
+  //return the solved rows as a 2d array of 9 x 9 
+
   // create rows
-  let r0 = puzzle[0];
-  let r1 = puzzle[1];
-  let r2 = puzzle[2];
-  let r3 = puzzle[3];
-  let r4 = puzzle[4];
-  let r5 = puzzle[5];
-  let r6 = puzzle[6];
-  let r7 = puzzle[7];
-  let r8 = puzzle[8];
+  let rows = puzzle;
 
   // create cols
-  let c0 = [];
-  let c1 = [];
-  let c2 = [];
-  let c3 = [];
-  let c4 = [];
-  let c5 = [];
-  let c6 = [];
-  let c7 = [];
-  let c8 = [];
-  puzzle.forEach( (v, i) => c0.push(puzzle[i][0]) );
-  puzzle.forEach( (v, i) => c1.push(puzzle[i][1]) );
-  puzzle.forEach( (v, i) => c2.push(puzzle[i][2]) );
-  puzzle.forEach( (v, i) => c3.push(puzzle[i][3]) );
-  puzzle.forEach( (v, i) => c4.push(puzzle[i][4]) );
-  puzzle.forEach( (v, i) => c5.push(puzzle[i][5]) );
-  puzzle.forEach( (v, i) => c6.push(puzzle[i][6]) );
-  puzzle.forEach( (v, i) => c7.push(puzzle[i][7]) );
-  puzzle.forEach( (v, i) => c8.push(puzzle[i][8]) );
+  let cols = [];
+  for (let i = 0; i <= 8; i++) {
+    for (let k = 0; k <= 8; k++) {
+      cols.push(rows[i][k]);  
+    
+    }
+  }
+  console.log(cols[0]);
+  
 
   // set rows counters to 0
   let r0Filleds = 0;
@@ -42,10 +25,6 @@ function sudoku(puzzle) {
   let r6Filleds = 0;
   let r7Filleds = 0;
   let r8Filleds = 0;
-  // count filleds for all rows
-  for(let i = 0; i <= 8; i++) {
-    
-  }
 
   // set cols counters to 0
   let c0Filleds = 0;
@@ -62,8 +41,6 @@ function sudoku(puzzle) {
 
 
 }
-
-
 
 var puzzle = [
   /* 0 */ [5,3,0,0,7,0,0,0,0],
