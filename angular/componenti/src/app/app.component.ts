6995536cfe2a87732bfee32a75f54bce2b354816
francky <francky@tuta.io>
2023-02-19 10:15:26 +0100

    usa ngfor

--- angular/componenti/src/app/app.component.ts
+++ angular/componenti/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 
+// interface Prodotto {
+//   quantita: number;
+//   nome: string;
+// }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   like: boolean = false;
   counter: number = 1;
   btnLike = 'LIKE';
+  prodotti = [
+    { quantita: 3, nome: 'mele' },
+    { quantita: 1, nome: 'pasta' },
+    { quantita: 1, nome: 'latte' },
+  ]
 
   increment() {
     this.counter += 1;
