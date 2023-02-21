93d33d475c607772b88c761c1ab06b8b27e9c2e5
francky <francky@tuta.io>
2023-02-21 23:01:33 +0100

    add components

--- angular/shopping-cart/src/app/product-list/product-list.component.ts
+++ angular/shopping-cart/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +11,12 @@ export class ProductListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
   }
 
 }
