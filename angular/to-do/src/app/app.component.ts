86f96009fe6d4d3fa43537618c1e7b1240bcc891
francky <francky@tuta.io>
2023-02-21 22:21:20 +0100

    add template for filtering

--- angular/to-do/src/app/app.component.ts
+++ angular/to-do/src/app/app.component.ts
@@ -3,33 +3,7 @@ import { Item } from './item';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="main">
-      <h1>My To Do List</h1>
-      <label for="addItemInput">What would you like to do today?</label>
-
-      <input id="addItemInput" placeholder="add item" class="lg-text-input"
-        #newItem
-        (keyup.enter)="addItem(newItem.value); newItem.value = ''"
-      />
-
-      <button class="btn-primary"
-      (click)="addItem(newItem.value); newItem.value = ''">
-        Add
-      </button>
-
-      <h2>{{items.length}} <span *ngIf="items.length === 1; else elseBlock">item</span>
-      <ng-template #elseBlock>items</ng-template></h2>
-
-      <ul>
-        <li *ngFor="let i of items">
-          <app-item (remove)="remove(i)" [item]="i"></app-item>
-        </li>
-      </ul>
-
-    </div>
-
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
