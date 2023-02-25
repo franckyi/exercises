06451e7ac5b69be9ed24f77377250231e39050f8
Francky Iside <francky@tuta.io>
2023-02-25 22:36:52 +0000

    aggiorna

--- angular/ripetizione-delle-basi/src/app/app.component.ts
+++ angular/ripetizione-delle-basi/src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ripetizione-delle-basi';
+  title: string = 'ripetizione-delle-basi';
+
+  url: string = 'http://www.go12345.com';
+
+  spesa: string[] = [ 'mela', 'pasta', 'pane', 'latte', 'vino' ];
+
+  counter: number = 1;
+
+  increment() {
+    this.counter++;
+  }
+
+  getLabel() {
+    if(this.counter === 0) return 'neutro';
+    return this.counter > 0 ? 'counter > 0' : 'counter < 0';
+  }
+
+  getClass() {
+    if (this.counter === 0) return null;
+    return this.counter > 0 ? 'green' : 'red';
+  }
+
+  click(text: string) {
+    alert(text)
+  }
+
+  mouseOver(): void {
+    alert('mouseOver');
+  }
+
+  change(): void {
+    alert('change')
+  }
+
 }
