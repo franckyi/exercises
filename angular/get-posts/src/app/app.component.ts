8f08178e8d1c93801cff23fce0ede035ec4d0abe
francky <francky@tuta.io>
2023-02-26 13:54:03 +0100

    in progress

--- angular/get-posts/src/app/app.component.ts
+++ angular/get-posts/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'get-posts';
+  public posts: any = [];
+
+  constructor(private _appService: AppService) {}
+
+  ngOnInit(): void {
+    this._appService.getPosts().subscribe( posts => {
+      this.posts = posts;
+    })
+  }
+
 }
---
+++ angular/get-posts/src/app/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getTodos() {
+    return this._httpClient.get('https://jsonplaceholder.typicode.com/posts');
+  }
+
+}
