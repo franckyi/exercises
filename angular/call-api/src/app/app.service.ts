38393934a39b0a8203088ca8f11f4e40d482a735
Francky Iside <francky@tuta.io>
2023-03-16 23:32:08 +0100

    import rxjs

--- angular/call-api/src/app/app.service.ts
+++ angular/call-api/src/app/app.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,16 @@ export class AppService {
 
   constructor(private _httpClient: HttpClient) { }
 
+  const getCoords = () => {
+
+}
+
+
   getUsers() {
     return this._httpClient.get('https://jsonplaceholder.typicode.com/users');
   }
+
+
+
 
 }
